a1ef886c9ca110042246bf2389c1c461b6e5cce5
JonahWalker <supposedlysam@gmail.com>
2018-09-09 20:02:50 -0600

    Adding a drawer with hamburger icon to the products page

--- lib/pages/products.dart
+++ lib/pages/products.dart
@@ -5,6 +5,13 @@ class ProductsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(child: Column(children: <Widget>[
+        AppBar(
+          title: Text("Menu"),
+          automaticallyImplyLeading: false // remove the hamburger icon from the drawer/AppBar
+        ),
+        ListTile(title: Text("Manage Products"), onTap: () => {},)
+      ],),),
       appBar: AppBar(
         title: Text('EasyList'),
       ),
