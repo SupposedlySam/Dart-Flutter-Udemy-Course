f06ff09cb604c5205e93a9225a99215321b1172b
JonahWalker <supposedlysam@gmail.com>
2018-09-09 20:37:41 -0600

    Adding tabs to the product admin page (todo: make tabs function)

--- lib/pages/product_admin.dart
+++ lib/pages/product_admin.dart
@@ -4,21 +4,34 @@ import './products.dart';
 class ProductAdminPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: Drawer(child: Column(children: <Widget>[
-        AppBar(
-          automaticallyImplyLeading: false,
-          title: Text("Options"),
+    return DefaultTabController(
+      length: 2, // Sets up the page to be tab controllable (still need to add tabs)
+      child: Scaffold(
+        drawer: Drawer(child: Column(children: <Widget>[
+          AppBar(
+            automaticallyImplyLeading: false,
+            title: Text("Options"),
+          ),
+          ListTile(
+            title: Text("All Products"),
+            onTap: () { Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) => ProductsPage(),)); } 
+          )
+        ],),),
+        appBar: AppBar(
+          title: Text('Product Admin'),
+          bottom: TabBar(tabs: <Widget>[
+            Tab(
+              icon: Icon(Icons.create),
+              text: 'Create Product'
+            ),
+            Tab(
+              icon: Icon(Icons.list),
+              text: 'My Products',
+            )
+          ],),
         ),
-        ListTile(
-          title: Text("Product Page"),
-          onTap: () { Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) => ProductsPage(),)); } 
-        )
-      ],),),
-      appBar: AppBar(
-        title: Text('Product Admin'),
+        body: Container(),
       ),
-      body: Container()
     );
   }
 }
